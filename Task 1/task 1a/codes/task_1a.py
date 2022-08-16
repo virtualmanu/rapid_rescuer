88e7fa673eca29ed4a7f19e7d8220f09486e2387
Manoj <26747363+virtualmanu@users.noreply.github.com>
2022-08-16 15:51:04 +0530

    initial

---
+++ Task 1/task 1a/codes/task_1a.py
@@ -0,0 +1,326 @@
+
+'''
+*****************************************************************************************
+*
+*        		===============================================
+*           		Rapid Rescuer (RR) Theme (eYRC 2019-20)
+*        		===============================================
+*
+*  This script is to implement Task 1A of Rapid Rescuer (RR) Theme (eYRC 2019-20).
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*  
+*  e-Yantra - An MHRD project under National Mission on Education using ICT (NMEICT)
+*
+*****************************************************************************************
+'''
+
+
+# Team ID:			[ Team-ID ]
+# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Filename:			task_1a.py
+# Functions:		readImage, solveMaze
+# 					[ Comma separated list of functions in this file ]
+# Global variables:	CELL_SIZE
+# 					[ List of global variables defined in this file ]
+
+
+# Import necessary modules
+# Do not import any other modules
+import cv2
+import numpy as np
+import os
+
+
+# To enhance the maze image
+import image_enhancer
+
+
+# Maze images in task_1a_images folder have cell size of 20 pixels
+CELL_SIZE = 20
+
+
+def readImage(img_file_path):
+
+	"""
+	Purpose:
+	---
+	the function takes file path of original image as argument and returns it's binary form
+
+	Input Arguments:
+	---
+	`img_file_path` :		[ str ]
+		file path of image
+
+	Returns:
+	---
+	`original_binary_img` :	[ numpy array ]
+		binary form of the original image at img_file_path
+
+	Example call:
+	---
+	original_binary_img = readImage(img_file_path)
+
+	"""
+
+	binary_img = None
+
+	#############	Add your Code here	###############
+
+	img = cv2.imread(img_file_path)
+	height, width = img.shape[:2]
+	print(height,width)
+
+	gray = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2GRAY)
+
+	'''r = 20.0 / gray.shape[1]
+	dim = (20, int(gray.shape[0] * r))
+	gray = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)'''
+
+	ret, binary_img = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+
+	
+	#trialnerror
+	'''for i in range (1,20):
+		for j in range (1,80):
+			binary_img[i][j] = 200
+			binary_img[i][j] = 200
+
+	for i in range (182,198):
+		binary_img[i][198] = 0
+		binary_img[i][199] = 0'''
+
+
+	gray = cv2.cvtColor(np.float32(binary_img), cv2.COLOR_GRAY2BGR)
+	cv2.imwrite("img.jpg", gray)
+
+	print(binary_img)
+	print (binary_img.size)
+	###################################################
+
+	return binary_img
+
+
+def solveMaze(original_binary_img, initial_point, final_point, no_cells_height, no_cells_width):
+
+	"""
+	Purpose:
+	---
+	the function takes binary form of original image, start and end point coordinates and solves the maze
+	to return the list of coordinates of shortest path from initial_point to final_point
+
+	Input Arguments:
+	---
+	`original_binary_img` :	[ numpy array ]
+		binary form of the original image at img_file_path
+	`initial_point` :		[ tuple ]
+		start point coordinates
+	`final_point` :			[ tuple ]
+		end point coordinates
+	`no_cells_height` :		[ int ]
+		number of cells in height of maze image
+	`no_cells_width` :		[ int ]
+		number of cells in width of maze image
+
+	Returns:
+	---
+	`shortestPath` :		[ list ]
+		list of coordinates of shortest path from initial_point to final_point
+
+	Example call:
+	---
+	shortestPath = solveMaze(original_binary_img, initial_point, final_point, no_cells_height, no_cells_width)
+
+	"""
+	
+	shortestPath = []
+
+	#############	Add your Code here	###############
+
+	startI = 
+
+
+	
+	#dijkstra
+	'''class Graph:
+	  def __init__(self):
+	    self.nodes = set()
+	    self.edges = defaultdict(list)
+	    self.distances = {}
+
+	  def add_node(self, value):
+	    self.nodes.add(value)
+
+	  def add_edge(self, from_node, to_node, distance):
+	    self.edges[from_node].append(to_node)
+	    self.edges[to_node].append(from_node)
+	    self.distances[(from_node, to_node)] = distance
+
+
+	def dijsktra(graph, initial):
+	  visited = {initial: 0}
+	  path = {}
+
+	  nodes = set(graph.nodes)
+
+	  while nodes: 
+	    min_node = None
+	    for node in nodes:
+	      if node in visited:
+	        if min_node is None:
+	          min_node = node
+	        elif visited[node] < visited[min_node]:
+	          min_node = node
+
+	    if min_node is None:
+	      break
+
+	    nodes.remove(min_node)
+	    current_weight = visited[min_node]
+
+	    for edge in graph.edges[min_node]:
+	      weight = current_weight + graph.distance[(min_node, edge)]
+	      if edge not in visited or weight < visited[edge]:
+	        visited[edge] = weight
+	        path[edge] = min_node
+
+	  return visited, path'''
+
+
+	###################################################
+	
+	return shortestPath 
+
+
+#############	You can add other helper functions here		#############
+
+
+
+#########################################################################
+
+
+# NOTE:	YOU ARE NOT ALLOWED TO MAKE ANY CHANGE TO THIS FUNCTION
+# 
+# Function Name:	main
+# Inputs:			None
+# Outputs: 			None
+# Purpose: 			the function first takes 'maze00.jpg' as input and solves the maze by calling readImage
+# 					and solveMaze functions, it then asks the user whether to repeat the same on all maze images
+# 					present in 'task_1a_images' folder or not
+
+if __name__ == '__main__':
+
+	curr_dir_path = os.getcwd()
+	img_dir_path = curr_dir_path + '/../task_1a_images/'				# path to directory of 'task_1a_images'
+	
+	file_num = 0
+	img_file_path = img_dir_path + 'maze0' + str(file_num) + '.jpg'		# path to 'maze00.jpg' image file
+
+	print('\n============================================')
+
+	print('\nFor maze0' + str(file_num) + '.jpg')
+
+	try:
+		
+		original_binary_img = readImage(img_file_path)
+		height, width = original_binary_img.shape
+
+	except AttributeError as attr_error:
+		
+		print('\n[ERROR] readImage function is not returning binary form of original image in expected format !\n')
+		exit()
+	
+	no_cells_height = int(height/CELL_SIZE)							# number of cells in height of maze image
+	no_cells_width = int(width/CELL_SIZE)							# number of cells in width of maze image
+	initial_point = (0, 0)											# start point coordinates of maze
+	final_point = ((no_cells_height-1),(no_cells_width-1))			# end point coordinates of maze
+
+	try:
+
+		shortestPath = solveMaze(original_binary_img, initial_point, final_point, no_cells_height, no_cells_width)
+
+		if len(shortestPath) > 2:
+
+			img = image_enhancer.highlightPath(original_binary_img, initial_point, final_point, shortestPath)
+			
+		else:
+
+			print('\n[ERROR] shortestPath returned by solveMaze function is not complete !\n')
+			exit()
+	
+	except TypeError as type_err:
+		
+		print('\n[ERROR] solveMaze function is not returning shortest path in maze image in expected format !\n')
+		exit()
+
+	print('\nShortest Path = %s \n\nLength of Path = %d' % (shortestPath, len(shortestPath)))
+	
+	print('\n============================================')
+	
+	cv2.imshow('canvas0' + str(file_num), img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+	choice = input('\nWant to run your script on all maze images ? ==>> "y" or "n": ')
+
+	if choice == 'y':
+
+		file_count = len(os.listdir(img_dir_path))
+
+		for file_num in range(file_count):
+
+			img_file_path = img_dir_path + 'maze0' + str(file_num) + '.jpg'
+
+			print('\n============================================')
+
+			print('\nFor maze0' + str(file_num) + '.jpg')
+
+			try:
+				
+				original_binary_img = readImage(img_file_path)
+				height, width = original_binary_img.shape
+
+			except AttributeError as attr_error:
+				
+				print('\n[ERROR] readImage function is not returning binary form of original image in expected format !\n')
+				exit()
+			
+			no_cells_height = int(height/CELL_SIZE)							# number of cells in height of maze image
+			no_cells_width = int(width/CELL_SIZE)							# number of cells in width of maze image
+			initial_point = (0, 0)											# start point coordinates of maze
+			final_point = ((no_cells_height-1),(no_cells_width-1))			# end point coordinates of maze
+
+			try:
+
+				shortestPath = solveMaze(original_binary_img, initial_point, final_point, no_cells_height, no_cells_width)
+
+				if len(shortestPath) > 2:
+
+					img = image_enhancer.highlightPath(original_binary_img, initial_point, final_point, shortestPath)
+					
+				else:
+
+					print('\n[ERROR] shortestPath returned by solveMaze function is not complete !\n')
+					exit()
+			
+			except TypeError as type_err:
+				
+				print('\n[ERROR] solveMaze function is not returning shortest path in maze image in expected format !\n')
+				exit()
+
+			print('\nShortest Path = %s \n\nLength of Path = %d' % (shortestPath, len(shortestPath)))
+			
+			print('\n============================================')
+
+			cv2.imshow('canvas0' + str(file_num), img)
+			cv2.waitKey(0)
+			cv2.destroyAllWindows()
+	
+	else:
+
+		print('')
+
+
